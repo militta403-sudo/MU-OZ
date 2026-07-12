f2d01ffff8fff2cd041ca97962b2a6dea6dbe8a5
MUÑOZ AREVALO MILAGROS LUCERO <militta.403@gmail.com>
2026-07-11 21:39:03 -0500

    Add files via upload

---
+++ SEMANA 3.R
@@ -0,0 +1,15 @@
+#semana 3 
+#datos de ejemplo (numero de casos)
+tuberculosis<-c(15,16,17,22,25)
+hepatitis<-c(18,12,11,16,10)
+diabetes<-c(25,38,22,28,20)
+
+#calcular el promedio
+mean(tuberculosis)
+mean(hepatitis)
+mean(diabetes)
+
+#calcular la desviacion
+sd(tuberculosis)
+sd(hepatitis)
+sd(diabetes)
